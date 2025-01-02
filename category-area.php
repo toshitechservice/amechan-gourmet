3a9b7dfe615378e32fe34ec38aa417d0f1562af9
t0sh1t0sh1 <tosh1syog1@yahoo.co.jp>
2025-01-03 08:27:44 +0900

    エリア一覧ページデザイン変更

--- category-area.php
+++ category-area.php
@@ -30,28 +30,46 @@
         </div>
       </div><!-- End Section Title -->
 
-      <div class="container">
+      <div class="container mb-5">
         <div class="row">
           <?php foreach ($posts as $post) : setup_postdata($post); ?>
+            <?php
+            $cat = get_the_category();
+            $catname = $cat[0]->cat_name;
+            ?>
             <div class="col-lg-4" data-aos="fade-up" data-aos-delay="100">
-              <div class="post-list border-bottom">
-                <a href="<?php the_permalink(); ?>">
-                  <?php
-                  if (has_post_thumbnail()) {
-                    the_post_thumbnail('full', array('class' => 'img-fluid'));
-                  } else {
-                    echo '<img src="' . get_template_directory_uri() . '/assets/img/post-landscape-2.jpg" alt="" class="img-fluid">';
-                  }
-                  ?>
-                </a>
-                <div class="post-meta">
-                  <span><?php echo get_the_date('Y/m/d'); ?></span>
-                </div>
-                <h2 class="mb-2">
+              <div class="post-list">
+                <div class="top-img-wrap overflow-hidden">
                   <a href="<?php the_permalink(); ?>">
-                    <?php echo wp_trim_words(get_the_title(), 12, '...'); ?>
+                    <?php
+                    if (has_post_thumbnail()) {
+                      the_post_thumbnail('full', array('class' => 'img-fluid area-post-img'));
+                    } else {
+                      echo '<img src="' . get_template_directory_uri() . '/assets/img/person-7.jpg" alt="" class="img-fluid area-post-img">';
+                    }
+                    ?>
                   </a>
-                </h2>
+                  <div class="post-meta">
+                    <span class="date post-tag-amechan"><?php echo esc_html($catname); ?></span>
+                    <span><?php echo get_the_date('Y/m/d'); ?></span>
+                  </div>
+                </div>
+                <div class="area-post-info-wrap">
+                  <h2><?php the_title(); ?></h2>
+                  <p class="top-excerpt">
+                    <?php
+                      if(has_excerpt()):
+                        echo get_the_excerpt();
+                      else:
+                        echo wp_trim_words( get_the_content() ,  50, '...');
+                      endif;
+                    ?>
+                  </p>
+
+                  <hr class="top-post-hr">
+
+                  <a href="<?php the_permalink(); ?>" class="top-readmore stretched-link"><span>続きを読む</span><i class="bi bi-arrow-right"></i></a>
+                </div>
               </div>
             </div>
           <?php endforeach; wp_reset_postdata(); ?>
