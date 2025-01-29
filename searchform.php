1eb132dbf029bb3d1a5009123c42341ed2b99b6c
t0sh1t0sh1 <tosh1syog1@yahoo.co.jp>
2025-01-29 17:22:03 +0900

    検索フォーム修正

--- searchform.php
+++ searchform.php
@@ -1,23 +1,22 @@
 <!-- Search Widget -->
 <div class="widget-item">
 
-  <form method="get" id="searchform" action="<?php echo esc_url(home_url('/')); ?>" class="amechan-form">
-    <label for="s" class="assistive-text widget-title">検索</label>
-    <div class="search-widget-amechan">
-      <input type="text" name="s" id="s" placeholder="未入力可" value="<?php echo isset($_GET['s']) ? esc_attr($_GET['s']) : ''; ?>"/>
-    </div>
+	<form method="get" id="searchform" action="<?php echo esc_url(home_url('/')); ?>" class="amechan-form">
+		<label for="s" class="assistive-text widget-title">絞り込み検索</label>
+		<div class="search-widget-amechan">
+			<input type="text" name="s" id="s" placeholder="例: ラーメン, ハンバーグ, 本町 など" value="<?php echo isset($_GET['s']) ? esc_attr($_GET['s']) : ''; ?>"/>
+		</div>
 
-    <div class="select-widget-amechan">
-      <select name="price">
-        <option value="" selected>価格帯絞り込み</option>
-        <option value="〜1999円">〜1999円</option>
-        <option value="2000円〜2999円">2000円〜2999円</option>
-        <option value="3000円〜">3000円〜</option>
-      </select>
-    </div>
-    <button type="submit" title="Search" class="amechan-button"><i class="bi bi-search"></i></button>
-    <p>※価格帯選択後に検索ボタン</p>
-  </form>
+		<div class="select-widget-amechan">
+			<select name="price">
+				<option value="" <?php echo empty($_GET['price']) ? 'selected' : ''; ?>>価格帯絞り込み</option>
+				<option value="〜1999円" <?php echo (isset($_GET['price']) && $_GET['price'] === '〜1999円') ? 'selected' : ''; ?>>〜1999円</option>
+				<option value="2000円〜2999円" <?php echo (isset($_GET['price']) && $_GET['price'] === '2000円〜2999円') ? 'selected' : ''; ?>>2000円〜2999円</option>
+				<option value="3000円〜" <?php echo (isset($_GET['price']) && $_GET['price'] === '3000円〜') ? 'selected' : ''; ?>>3000円〜</option>
+			</select>
+		</div>
+		<button type="submit" title="Search" class="amechan-button">検索</button>
+	</form>
 
 </div>
 
